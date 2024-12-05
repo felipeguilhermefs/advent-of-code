f3e8087c81fc6644f417942b26eeba0516a20d2a
Felipe Flores <felipeguilhermefs@gmail.com>
2024-12-05 17:08:59 +0100

    Solve day 5 part 2

--- 2024/day5.lua
+++ 2024/day5.lua
@@ -1,25 +1,21 @@
+local HashMap = require("ff.collections.hashmap")
+
 local function Node(value)
-	return { value = value, children = {} }
+	return { value = value, children = HashMap.new() }
 end
 
 local function addNodes(nodes, prev, next)
-	if nodes[prev] == nil then
-		nodes[prev] = Node(prev)
-	end
-	local prevNode = nodes[prev]
+	local prevNode = nodes:compute(prev, Node)
+	local nextNode = nodes:compute(next, Node)
 
-	if nodes[next] == nil then
-		nodes[next] = Node(next)
-	end
-	local nextNode = nodes[next]
-
-	prevNode.children[next] = nextNode
+	prevNode.children:put(next, nextNode)
 end
 
 local function findSafeMiddle(nodes, line)
 	local pages = {}
+
 	for value in line:gmatch("(%d+)") do
-		local node = nodes[value]
+		local node = nodes:get(value)
 		if node == nil then
 			return 0
 		end
@@ -28,15 +24,12 @@ local function findSafeMiddle(nodes, line)
 		nodes = node.children
 	end
 
-	if #pages % 2 == 0 then
-		print("DANGER!!! EVEN !!!")
-	end
 	return pages[math.floor(#pages / 2) + 1]
 end
 
 local function part1()
 	local sum = 0
-	local nodes = {}
+	local nodes = HashMap.new()
 	for line in io.lines(arg[1]) do
 		local prev, next = line:match("(%d+)|(%d+)")
 		if prev then
@@ -45,7 +38,53 @@ local function part1()
 			sum = sum + findSafeMiddle(nodes, line)
 		end
 	end
+
+	return sum
+end
+
+local function findUnsafeMiddle(nodes, line)
+	local pages = {}
+
+	for value in line:gmatch("(%d+)") do
+		local node = nodes:get(value)
+		table.insert(pages, node)
+	end
+
+	local safe = true
+	for _, page in pairs(pages) do
+		local node = nodes:get(page.value)
+		if node == nil then
+			safe = false
+			break
+		end
+		nodes = node.children
+	end
+
+	if safe then
+		return 0
+	end
+
+	table.sort(pages, function(a, b)
+		return a.children:contains(b.value)
+	end)
+
+	return pages[math.floor(#pages / 2) + 1].value
+end
+
+local function part2()
+	local sum = 0
+	local nodes = HashMap.new()
+	for line in io.lines(arg[1]) do
+		local prev, next = line:match("(%d+)|(%d+)")
+		if prev then
+			addNodes(nodes, prev, next)
+		elseif line ~= "" then
+			sum = sum + findUnsafeMiddle(nodes, line)
+		end
+	end
+
 	return sum
 end
 
 print("Part 1", part1())
+print("Part 2", part2())
