b482ccb8609469030c7bde5ceca1a5c12662bea6
Felipe Flores <felipeguilhermefs@gmail.com>
2024-12-06 11:12:40 +0100

    Solve day 6 part 2

--- 2024/day06.lua
+++ 2024/day06.lua
@@ -38,13 +38,29 @@ local function inGrid(row, col)
 	return true
 end
 
-local function findInitialPos()
+local function copyGrid()
+	local grid = {}
+	for _, row in pairs(GRID) do
+		local newRow = {}
+		for _, pos in pairs(row) do
+			table.insert(newRow, pos)
+		end
+		table.insert(grid, newRow)
+	end
+	return grid
+end
+
+local function newGuard(row, col, dir)
+	return { row = row, col = col, dir = dir }
+end
+
+local function findGuard()
 	local dir
 	for row, _ in pairs(GRID) do
 		for col, pos in pairs(GRID[row]) do
 			dir = DIR[pos]
 			if dir then
-				return { row = row, col = col, dir = dir }
+				return newGuard(row, col, dir)
 			end
 		end
 	end
@@ -52,25 +68,89 @@ local function findInitialPos()
 end
 
 local function part1()
-	local pos = assert(findInitialPos())
+	local guard = assert(findGuard())
+	local grid = copyGrid()
 
-	local totalPos = 1
+	local positions = 1
 	while true do
-		local nextRow, nextCol = pos.row + pos.dir[1], pos.col + pos.dir[2]
+		local nextRow, nextCol = guard.row + guard.dir[1], guard.col + guard.dir[2]
 		if not inGrid(nextRow, nextCol) then
-			return totalPos
+			return positions
 		end
 
-		if GRID[nextRow][nextCol] == "#" then
-			pos.dir = pos.dir.next
+		if grid[nextRow][nextCol] == "#" then
+			guard.dir = guard.dir.next
 		end
-		if GRID[pos.row][pos.col] ~= "X" then
-			GRID[pos.row][pos.col] = "X"
-			totalPos = totalPos + 1
+		if grid[guard.row][guard.col] ~= "X" then
+			grid[guard.row][guard.col] = "X"
+			positions = positions + 1
 		end
-		pos.row = pos.row + pos.dir[1]
-		pos.col = pos.col + pos.dir[2]
+		guard.row = guard.row + guard.dir[1]
+		guard.col = guard.col + guard.dir[2]
 	end
 end
 
+local function isLoop(grid, guard)
+	local limit = #grid * #grid[1]
+	for _ = 1, limit do
+		local nextRow, nextCol = guard.row + guard.dir[1], guard.col + guard.dir[2]
+		if not inGrid(nextRow, nextCol) then
+			return false
+		end
+
+		if grid[nextRow][nextCol] == "#" then
+			guard.dir = guard.dir.next
+		end
+		guard.row = guard.row + guard.dir[1]
+		guard.col = guard.col + guard.dir[2]
+	end
+	return true
+end
+
+local function findGuardPath()
+	local guard = assert(findGuard())
+	local grid = copyGrid()
+
+	local positions = {}
+	while true do
+		local nextRow, nextCol = guard.row + guard.dir[1], guard.col + guard.dir[2]
+
+		if positions[guard.row] == nil then
+			positions[guard.row] = {}
+		end
+		positions[guard.row][guard.col] = true
+
+		if not inGrid(nextRow, nextCol) then
+			return positions
+		end
+
+		if grid[nextRow][nextCol] == "#" then
+			guard.dir = guard.dir.next
+		end
+		guard.row = guard.row + guard.dir[1]
+		guard.col = guard.col + guard.dir[2]
+	end
+end
+
+local function part2()
+	local guard = assert(findGuard())
+	local positions = findGuardPath()
+	local count = 0
+	for row, _ in pairs(positions) do
+		for col, _ in pairs(positions[row]) do
+			if row == guard.row and col == guard.col then
+				goto continue
+			end
+			local grid = copyGrid()
+			grid[row][col] = "#"
+			if isLoop(grid, newGuard(guard.row, guard.col, guard.dir)) then
+				count = count + 1
+			end
+			::continue::
+		end
+	end
+	return count
+end
+
 print("Part 1", part1())
+print("Part 2", part2())
