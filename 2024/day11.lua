3b34a44652a2675841c47d26e00589c5596a592a
Felipe Flores <felipeguilhermefs@gmail.com>
2024-12-11 12:23:36 +0100

    Simplify day 11

--- 2024/day11.lua
+++ 2024/day11.lua
@@ -1,60 +1,43 @@
+local function addCount(counter, key, value)
+	counter[key] = (counter[key] or 0) + value
+end
+
 local function readInput()
 	local f = assert(io.open(arg[1], "rb"))
 	local content = f:read("*a")
 	f:close()
 
-	local stones = {}
+	local counter = {}
 	for num in content:gmatch("%d+") do
-		stones[num] = (stones[num] or 0) + 1
+		addCount(counter, num, 1)
 	end
-	return stones
+	return counter
 end
 
-local function shift(stones)
-	local cur = stones.front
-
-	while cur.next ~= stones.back do
-		cur = cur.next
-		if cur.value == 0 then
-			cur.value = 1
-			goto continue
-		end
-
-		local engraved = tostring(cur.value)
-		if #engraved % 2 == 0 then
-			local first = tonumber(engraved:sub(1, math.floor(#engraved / 2)))
-			stones.pushBefore(cur, first)
-
-			local second = tonumber(engraved:sub(math.floor(#engraved / 2) + 1, #engraved))
-			cur.value = second
-		else
-			cur.value = cur.value * 2024
-		end
-
-		::continue::
-	end
+local function split(num)
+	local first = tonumber(num:sub(1, #num // 2))
+	local second = tonumber(num:sub(#num // 2 + 1, #num))
+	return tostring(first), tostring(second)
 end
 
 local function run(times)
 	local stones = readInput()
 
 	for _ = 1, times do
-		local cache = {}
+		local counter = {}
 		for stone, count in pairs(stones) do
 			if stone == "0" then
-				cache["1"] = (cache["1"] or 0) + count
+				addCount(counter, "1", count)
 			elseif #stone % 2 == 0 then
-				local first = tostring(tonumber(stone:sub(1, #stone // 2)))
-				cache[first] = (cache[first] or 0) + count
-				local second = tostring(tonumber(stone:sub(#stone // 2 + 1, #stone)))
-				cache[second] = (cache[second] or 0) + count
+				local first, second = split(stone)
+				addCount(counter, first, count)
+				addCount(counter, second, count)
 			else
-				local num = tostring(tonumber(stone) * 2024)
-				cache[num] = (cache[num] or 0) + count
+				addCount(counter, tostring(tonumber(stone) * 2024), count)
 			end
 		end
 
-		stones = cache
+		stones = counter
 	end
 
 	local total = 0
