f7b4851f240a561a687669a173f42d8d5afd380e
Felipe Flores <felipeguilhermefs@gmail.com>
2024-12-14 19:12:20 +0100

    Solve day 14 part 2

--- 2024/day14.lua
+++ 2024/day14.lua
@@ -1,3 +1,5 @@
+local Set = require("ff.collections.set")
+
 local HEIGHT = 103
 local WIDTH = 101
 
@@ -16,17 +18,16 @@ local function readInput()
 	return robots
 end
 
-local function move(pos, vel, limit)
-	return (pos + vel * 100) % limit
+local function move(pos, vel, limit, times)
+	times = times or 1
+	return (pos + vel * times) % limit
 end
 
-local function run()
-	local robots = readInput()
-
+local function calculateSafety(robots)
 	local quadrants = { 0, 0, 0, 0 }
 	for _, robot in pairs(robots) do
-		local x = move(robot[1], robot[3], WIDTH)
-		local y = move(robot[2], robot[4], HEIGHT)
+		local x = move(robot[1], robot[3], WIDTH, 100)
+		local y = move(robot[2], robot[4], HEIGHT, 100)
 
 		if x < HALF_W and y < HALF_H then
 			quadrants[1] = quadrants[1] + 1
@@ -43,8 +44,32 @@ local function run()
 	for _, count in pairs(quadrants) do
 		safetyFactor = safetyFactor * count
 	end
+	return safetyFactor
+end
 
-	return safetyFactor, 0
+local function untilXmasTree(robots)
+	local seconds = 1
+
+	while true do
+		local visited = Set.new()
+		for i, robot in ipairs(robots) do
+			robots[i][1] = move(robot[1], robot[3], WIDTH)
+			robots[i][2] = move(robot[2], robot[4], HEIGHT)
+			visited:add(string.format("%d:%d", robots[i][1], robots[i][2]))
+		end
+
+		if #visited == #robots then
+			return seconds
+		end
+
+		seconds = seconds + 1
+	end
+end
+
+local function run()
+	local robots = readInput()
+
+	return calculateSafety(robots), untilXmasTree(robots)
 end
 
 local part1, part2 = run()
