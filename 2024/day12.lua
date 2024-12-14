fe8d43221600ab8b021480754c818c624129ca45
Felipe Flores <felipeguilhermefs@gmail.com>
2024-12-14 13:54:30 +0100

    Simplify day 12

--- 2024/day12.lua
+++ 2024/day12.lua
@@ -1,4 +1,3 @@
-local HashMap = require("ff.collections.hashmap")
 local Set = require("ff.collections.set")
 local Queue = require("ff.collections.queue")
 
@@ -11,7 +10,13 @@ local SW = { 1, -1 }
 local W = { 0, -1 }
 local NW = { -1, -1 }
 
+NE.outer = { N, E }
+NW.outer = { N, W }
+SE.outer = { S, E }
+SW.outer = { S, W }
+
 local DIRECTIONS = { N, E, S, W }
+local OUTER_DIRECTIONS = {NE, NW, SE, SW}
 
 local function readInput()
 	local map = {}
@@ -41,54 +46,41 @@ local function id(row, col)
 	return string.format("%d:%d", row, col)
 end
 
-local function countCorners(map, row, col, neighbors)
-	if #neighbors == 0 then
+local function Node(row, col, dir)
+	return { row = row, col = col, id = id(row, col), dir = dir }
+end
+
+local function isOuterCorner(map, row, col, dir, neighborsDir)
+	local plant = map[row][col]
+	local oRow, oCol =  row + dir[1], col + dir[2]
+
+	if inMap(map, oRow, oCol) and map[oRow][oCol] ~= plant then
+		return neighborsDir:contains(dir.outer[1]) and neighborsDir:contains(dir.outer[2])
+	end
+	return false
+
+end
+
+local function countCorners(map, row, col, neighborsDir)
+	if #neighborsDir == 0 then
 		return 4
 	end
 
-	if #neighbors == 1 then
+	if #neighborsDir == 1 then
 		return 2
 	end
 
 	local corners = 0
 
-	local nDirections = Set.new()
-	for _, neighbor in pairs(neighbors) do
-		nDirections:add(neighbor[4])
-	end
-
-	if #neighbors == 2 then
-		if not (nDirections:contains(N) and nDirections:contains(S)) and not (nDirections:contains(E) and nDirections:contains(W)) then
+	if #neighborsDir == 2 then
+		if not (neighborsDir:contains(N) and neighborsDir:contains(S)) and not (neighborsDir:contains(E) and neighborsDir:contains(W)) then
+			-- perpendicular
 			corners = 1
 		end
 	end
 
-	local plant = map[row][col]
-
-	local oRow, oCol =  row + NE[1], col + NE[2]
-	if inMap(map, oRow, oCol) and map[oRow][oCol] ~= plant then
-		if nDirections:contains(N) and nDirections:contains(E) then
-			corners = corners + 1
-		end
-	end
-
-	oRow, oCol =  row + NW[1], col + NW[2]
-	if inMap(map, oRow, oCol) and map[oRow][oCol] ~= plant then
-		if nDirections:contains(N) and nDirections:contains(W) then
-			corners = corners + 1
-		end
-	end
-
-	oRow, oCol =  row + SE[1], col + SE[2]
-	if inMap(map, oRow, oCol) and map[oRow][oCol] ~= plant then
-		if nDirections:contains(S) and nDirections:contains(E) then
-			corners = corners + 1
-		end
-	end
-
-	oRow, oCol =  row + SW[1], col + SW[2]
-	if inMap(map, oRow, oCol) and map[oRow][oCol] ~= plant then
-		if nDirections:contains(S) and nDirections:contains(W) then
+	for _, dir in pairs(OUTER_DIRECTIONS) do
+		if isOuterCorner(map, row, col, dir, neighborsDir) then
 			corners = corners + 1
 		end
 	end
@@ -103,28 +95,28 @@ local function determineRegions(map, row, col)
 	local corners = 0
 
 	local toMap = Queue.new()
-	toMap:enqueue({ row, col, id(row, col) })
+	toMap:enqueue(Node(row, col))
 
 	while not toMap:empty() do
 		local cur = toMap:dequeue()
-		if not area:add(cur[3]) then
+		if not area:add(cur.id) then
 			goto continue
 		end
 
-		local neighbors = {}
+		local neighborsDir = Set.new()
 		for _, dir in pairs(DIRECTIONS) do
-			local nRow, nCol = cur[1] + dir[1], cur[2] + dir[2]
+			local nRow, nCol = cur.row + dir[1], cur.col + dir[2]
 			if inMap(map, nRow, nCol) and map[nRow][nCol] == plant then
-				local neighbor = { nRow, nCol, id(nRow, nCol), dir }
-				if not area:contains(neighbor[3]) then
+				local neighbor = Node(nRow, nCol, dir)
+				if not area:contains(neighbor.id) then
 					toMap:enqueue(neighbor)
 				end
-				table.insert(neighbors, neighbor)
+				neighborsDir:add(dir)
 			end
 		end
 
-		perimeter = perimeter + (4 - #neighbors)
-		corners = corners + countCorners(map, cur[1], cur[2], neighbors)
+		perimeter = perimeter + (4 - #neighborsDir)
+		corners = corners + countCorners(map, cur.row, cur.col, neighborsDir)
 
 		::continue::
 	end
@@ -148,6 +140,7 @@ local function run()
 			total = total + (perimeter * #area)
 			withDiscount = withDiscount + (corners * #area)
 
+			-- Add everything from area into mapped, so we skip them
 			for pos, _ in pairs(area._entries) do
 				mapped:add(pos)
 			end
@@ -159,5 +152,5 @@ local function run()
 end
 
 local part1, part2 = run()
-print("Part 1", part1, part1 == 1573474)
-print("Part 2", part2, part2 == 966476)
+print("Part 1", part1)
+print("Part 2", part2)
